8ceb2ecf7c754f4154fe700df43a671c7d27752b
ParisotRomain <parisot.romain54@gmail.com>
2019-05-17 21:03:24 +0200

    first commit

---
+++ src/main/java/com/adaming/projetformationlille/config/CacheConfiguration.java
@@ -0,0 +1,65 @@
+package com.adaming.projetformationlille.config;
+
+import java.time.Duration;
+
+import org.ehcache.config.builders.*;
+import org.ehcache.jsr107.Eh107Configuration;
+
+import io.github.jhipster.config.jcache.BeanClassLoaderAwareJCacheRegionFactory;
+import io.github.jhipster.config.JHipsterProperties;
+
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+    private final javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration;
+
+    public CacheConfiguration(JHipsterProperties jHipsterProperties) {
+        BeanClassLoaderAwareJCacheRegionFactory.setBeanClassLoader(this.getClass().getClassLoader());
+        JHipsterProperties.Cache.Ehcache ehcache =
+            jHipsterProperties.getCache().getEhcache();
+
+        jcacheConfiguration = Eh107Configuration.fromEhcacheCacheConfiguration(
+            CacheConfigurationBuilder.newCacheConfigurationBuilder(Object.class, Object.class,
+                ResourcePoolsBuilder.heap(ehcache.getMaxEntries()))
+                .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofSeconds(ehcache.getTimeToLiveSeconds())))
+                .build());
+    }
+
+    @Bean
+    public JCacheManagerCustomizer cacheManagerCustomizer() {
+        return cm -> {
+            cm.createCache(com.adaming.projetformationlille.repository.UserRepository.USERS_BY_LOGIN_CACHE, jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.repository.UserRepository.USERS_BY_EMAIL_CACHE, jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.User.class.getName(), jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Authority.class.getName(), jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Projet.class.getName(), jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Projet.class.getName() + ".commentaires", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Projet.class.getName() + ".news", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Projet.class.getName() + ".profilContributeurs", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Projet.class.getName() + ".profilSuiveurs", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Projet.class.getName() + ".categories", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Profil.class.getName(), jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Profil.class.getName() + ".projetPortes", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Profil.class.getName() + ".projetContribues", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Profil.class.getName() + ".projetSuivis", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Dons.class.getName(), jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Commentaire.class.getName(), jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Categorie.class.getName(), jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Categorie.class.getName() + ".projets", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.News.class.getName(), jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Projet.class.getName() + ".profilsContributeurs", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Projet.class.getName() + ".profilsSuiveurs", jcacheConfiguration);
+            cm.createCache(com.adaming.projetformationlille.domain.Profil.class.getName() + ".projetsPortes", jcacheConfiguration);
+            // jhipster-needle-ehcache-add-entry
+        };
+    }
+}
---
+++ src/main/webapp/app/entities/entity.module.ts
@@ -0,0 +1,135 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+    imports: [
+        RouterModule.forChild([
+            {
+                path: 'projet',
+                loadChildren: './projet/projet.module#CrowdCodileProjetModule'
+            },
+            {
+                path: 'profil',
+                loadChildren: './profil/profil.module#CrowdCodileProfilModule'
+            },
+            {
+                path: 'dons',
+                loadChildren: './dons/dons.module#CrowdCodileDonsModule'
+            },
+            {
+                path: 'commentaire',
+                loadChildren: './commentaire/commentaire.module#CrowdCodileCommentaireModule'
+            },
+            {
+                path: 'categorie',
+                loadChildren: './categorie/categorie.module#CrowdCodileCategorieModule'
+            },
+            {
+                path: 'news',
+                loadChildren: './news/news.module#CrowdCodileNewsModule'
+            },
+            {
+                path: 'projet',
+                loadChildren: './projet/projet.module#CrowdCodileProjetModule'
+            },
+            {
+                path: 'profil',
+                loadChildren: './profil/profil.module#CrowdCodileProfilModule'
+            },
+            {
+                path: 'dons',
+                loadChildren: './dons/dons.module#CrowdCodileDonsModule'
+            },
+            {
+                path: 'commentaire',
+                loadChildren: './commentaire/commentaire.module#CrowdCodileCommentaireModule'
+            },
+            {
+                path: 'categorie',
+                loadChildren: './categorie/categorie.module#CrowdCodileCategorieModule'
+            },
+            {
+                path: 'news',
+                loadChildren: './news/news.module#CrowdCodileNewsModule'
+            },
+            {
+                path: 'projet',
+                loadChildren: './projet/projet.module#CrowdCodileProjetModule'
+            },
+            {
+                path: 'profil',
+                loadChildren: './profil/profil.module#CrowdCodileProfilModule'
+            },
+            {
+                path: 'dons',
+                loadChildren: './dons/dons.module#CrowdCodileDonsModule'
+            },
+            {
+                path: 'commentaire',
+                loadChildren: './commentaire/commentaire.module#CrowdCodileCommentaireModule'
+            },
+            {
+                path: 'categorie',
+                loadChildren: './categorie/categorie.module#CrowdCodileCategorieModule'
+            },
+            {
+                path: 'news',
+                loadChildren: './news/news.module#CrowdCodileNewsModule'
+            },
+            {
+                path: 'projet',
+                loadChildren: './projet/projet.module#CrowdCodileProjetModule'
+            },
+            {
+                path: 'profil',
+                loadChildren: './profil/profil.module#CrowdCodileProfilModule'
+            },
+            {
+                path: 'dons',
+                loadChildren: './dons/dons.module#CrowdCodileDonsModule'
+            },
+            {
+                path: 'commentaire',
+                loadChildren: './commentaire/commentaire.module#CrowdCodileCommentaireModule'
+            },
+            {
+                path: 'categorie',
+                loadChildren: './categorie/categorie.module#CrowdCodileCategorieModule'
+            },
+            {
+                path: 'news',
+                loadChildren: './news/news.module#CrowdCodileNewsModule'
+            },
+            {
+                path: 'projet',
+                loadChildren: './projet/projet.module#CrowdCodileProjetModule'
+            },
+            {
+                path: 'profil',
+                loadChildren: './profil/profil.module#CrowdCodileProfilModule'
+            },
+            {
+                path: 'dons',
+                loadChildren: './dons/dons.module#CrowdCodileDonsModule'
+            },
+            {
+                path: 'commentaire',
+                loadChildren: './commentaire/commentaire.module#CrowdCodileCommentaireModule'
+            },
+            {
+                path: 'categorie',
+                loadChildren: './categorie/categorie.module#CrowdCodileCategorieModule'
+            },
+            {
+                path: 'news',
+                loadChildren: './news/news.module#CrowdCodileNewsModule'
+            }
+            /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
+        ])
+    ],
+    declarations: [],
+    entryComponents: [],
+    providers: [],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class CrowdCodileEntityModule {}
